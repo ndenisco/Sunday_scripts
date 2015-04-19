7a954261631ac53fae44cfb333549b62e53ea293
Nicole De Nisco <nicole.denisco@gmail.com>
2015-04-19 14:00:09 -0500

    playing with clit functions

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -5,3 +5,17 @@
 
 # Read in data as tab-delimited
 gap.in <- read.table("output/combined_gapMinder.tsv", sep ="\t", header=TRUE)
+
+for (row.number in 1:10){
+  for (col.number in 1:5){
+    print(gap.in[row.number, col.number])
+  }
+}
+
+gap.in[1:10,1:5]
+
+add.me<-function(x,y){
+  x+y
+}
+
+add.me(3,4)
